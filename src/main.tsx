cb2ce2cd3c91e4806d32170827f1668acac15c2e
Alireza Chizari <ch.alireza021@gmail.com>
2024-08-16 14:06:52 +0330

    install stylis and stylis-plugin-rtl package

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import App from "./App.tsx";
 import "./index.css";
 import { theme } from "./theme";
 import { ThemeProvider } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
   </StrictMode>
 );
