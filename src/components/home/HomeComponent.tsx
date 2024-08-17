ff8f6390977a484c31ebfdac125f9e83b40addc5
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-17 12:01:23 +0330

    feat:home

---
+++ src/components/home/HomeComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AdvertisingHeader } from "./slider";
+
+export const HomeComponent = () => {
+  return (
+    <>
+      <div>HomeComponent</div>
+      <AdvertisingHeader/>
+    </>
+  );
+};
