922e23b6e11629f492f6ddb8035648e9a73977b9
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-18 17:16:42 +0330

    login route

--- src/config/router/routerConfig.ts
+++ src/config/router/routerConfig.ts
@@ -1,9 +1,8 @@
 export const PATH = {
   HOME: "/",
-  TERMS:"terms",
-  ABOUT_US:"about-us",
-  COMMON_QUESTION:"common-questions",
-  LOGIN:"login",
+  TERMS: "terms",
+  ABOUT_US: "about-us",
+  COMMON_QUESTION: "common-questions",
+  LOGIN: "login",
+  SIGN_UP: "sign-up",
 };
-
-
---
+++ src/config/url.ts
@@ -0,0 +1,11 @@
+
+export const URL_LOGIN="auth/login"
+export const URL_SIGNUP="auth/signup"
+export const URL_REFRESH_TOKEN="auth/token";
+export const URL_PRODUCT="products";
+export const URL_ORDERS="orders";
+export const URL_CATEGORY="categories";
+export const URL_SUBCATEGORY="subcategories";
+export const URL_USER="users";
+export const URL_BACKEND_IMAGES="http://localhost:8000/images/products";
+export const URL_BACKEND_THUMBNAILS="http://localhost:8000/images/products/thumbnails/";
