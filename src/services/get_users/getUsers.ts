4e52a88a7b9746682e46294c6044409c47fe4776
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-23 23:31:02 +0330

    improve get user func

--- src/services/get_users/getUsers.ts
+++ src/services/get_users/getUsers.ts
@@ -4,10 +4,14 @@ import { api } from "../interceptors";
 export const getUsers = async ({
   page,
   limit,
+  orderBy,
+  order
 }: {
   page: number;
   limit: number;
+  orderBy:string;
+  order:string;
 }) => {
-  const respons = await api.get(`${URL_USER}?page=${page}&limit=${limit}`);
+  const respons = await api.get(`${URL_USER}?page=${page}&limit=${limit}&sort=${order==='asc'? '':'-'}${orderBy}`);
   return respons.data;
 };
