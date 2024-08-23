3f33f85e82518f069e64e2ef33956f6e19a99995
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-23 18:06:31 +0330

    feat:react query

--- src/main.tsx
+++ src/main.tsx
@@ -9,19 +9,25 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const cacheRtl = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
+const client = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false }, mutations: {} },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CacheProvider value={cacheRtl}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </CacheProvider>
   </StrictMode>
 );
