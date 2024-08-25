86b195875f5c219374c09772071bdc66f57c84f1
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-25 14:56:27 +0330

    order table header

---
+++ src/pages/admin/admin_orders/components/OrdersHeaders.tsx
@@ -0,0 +1,52 @@
+import {
+  TableHead,
+  TableRow,
+  TableCell,
+  TableSortLabel,
+  Box,
+} from "@mui/material";
+import { EnhancedTablePropsIF, OrdersOrderBy, header } from "../utils";
+import { visuallyHidden } from "@mui/utils";
+
+export const OrdersHeaders = (props: EnhancedTablePropsIF) => {
+  const { order, orderBy, onRequestSort } = props;
+
+  const createSortHandler =
+    (property: OrdersOrderBy) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+  const sort: OrdersOrderBy[] = ["createdAt", "totalPrice"];
+  return (
+    <TableHead>
+      <TableRow>
+        {header.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={"left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            {sort.includes(headCell.id as OrdersOrderBy) ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : "asc"}
+                onClick={createSortHandler(headCell.id as OrdersOrderBy)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === "desc"
+                      ? "sorted descending"
+                      : "sorted ascending"}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            ) : (
+              <>{headCell.label}</>
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
