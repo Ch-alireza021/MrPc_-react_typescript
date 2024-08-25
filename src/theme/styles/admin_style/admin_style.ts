43bdaea7b669cc0779a8ae3e5d1baa3ab890c907
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-25 22:18:23 +0330

    update admin style

--- src/theme/styles/admin_style/admin_style.ts
+++ src/theme/styles/admin_style/admin_style.ts
@@ -8,8 +8,10 @@ export const HoverRevealText = styled.div`
   transform: translate(0, -10px);
   z-index: 999;
   background-color: white;
-  padding: 10px 20px ;
+  padding: 10px 30px ;
   white-space: nowrap;
+  border-radius:1rem;
+  font-size: large;
 `;
 
 export const HoverRevealCustomBox = styled.div`
