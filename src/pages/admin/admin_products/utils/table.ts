5a685d35631d8dd939f67cba217624b139969444
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-25 19:07:01 +0330

    table header data

---
+++ src/pages/admin/admin_products/utils/table.ts
@@ -0,0 +1,46 @@
+import {  HeadCellIF } from "./interface";
+  
+ export  const header: readonly HeadCellIF[] = [
+    {
+      id: "thumbnail",
+      numeric: false,
+      disablePadding: true,
+      label: " تصویر کالا",
+    },
+    {
+      id: "name",
+      numeric: false,
+      disablePadding: true,
+      label: " نام کالا",
+    },
+    {
+      id: "category",
+      numeric: false,
+      disablePadding: false,
+      label: " دسته بندی",
+    },
+    {
+      id: "subcategory",
+      numeric: false,
+      disablePadding: false,
+      label: " زیر مجموعه ",
+    },
+    {
+      id: "price",
+      numeric: false,
+      disablePadding: false,
+      label: "  قیمت",
+    },
+    {
+      id: "updatedAt",
+      numeric: true,
+      disablePadding: false,
+      label: " به روز رسانی ",
+    },
+    {
+      id: "quantity",
+      numeric: false,
+      disablePadding: false,
+      label: " موجودی  ",
+    },
+  ];
