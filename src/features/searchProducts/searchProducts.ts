98ab03d2d123916e2e9e0a277a5cd8cffd8a93b3
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-26 21:03:45 +0330

    feat:search

---
+++ src/features/searchProducts/searchProducts.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface SearchProductsState {
+  category: string;
+  subcategory: string;
+  quantity: number | null;
+  price: number | null;
+  req: boolean;
+}
+
+const initialState: SearchProductsState = {
+  category: "",
+  subcategory: "",
+  quantity: null,
+  price: null,
+  req: false,
+};
+
+const searchProducts = createSlice({
+  name: "searchProducts",
+  initialState,
+  reducers: {
+    setCategory: (state, action: PayloadAction<string>) => {
+      state.category = action.payload;
+    },
+    setSubcategory: (state, action: PayloadAction<string>) => {
+      state.subcategory = action.payload;
+    },
+    setQuantity: (state, action: PayloadAction<number>) => {
+      state.quantity = action.payload;
+    },
+    setPrice: (state, action: PayloadAction<number>) => {
+      state.price = action.payload;
+    },
+    setReq: (state, action: PayloadAction<boolean>) => {
+      state.req = action.payload;
+    },
+  },
+});
+
+export const { setCategory, setSubcategory, setQuantity, setPrice, setReq } =
+  searchProducts.actions;
+export default searchProducts.reducer;
--- src/pages/admin/admin_products/components/search/APSearch.tsx
+++ src/pages/admin/admin_products/components/search/APSearch.tsx
@@ -1,35 +1,76 @@
-import { useState } from "react";
-import { Box, Typography } from "@mui/material";
-import { Input as BaseInput } from "@mui/base/Input";
-import { InputElement, InputRoot } from "../../../../../theme";
-import SelectOption from "./SelectOption";
+import { URL_CATEGORY, URL_SUBCATEGORY } from "../../../../../config";
+import { SelectOption } from "./SelectOption";
+import { Box, Button, TextField } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCategory,
+  setPrice,
+  setQuantity,
+  setReq,
+  setSubcategory,
+} from "../../../../../features";
+import { RootState } from "../../../../../features/rootReducers";
 
 // APSearch ==> admin products search
 export const APSearch = () => {
-  const [formValues, setFormValues] = useState({ categories: "" });
+  const dispatch = useDispatch();
+  const formValues = useSelector((state: RootState) => state?.sPState);
+  const handleSubmit = () => {
+    dispatch(setReq(true));
+  };
   return (
-    <form>
+    <Box style={{ display: "flex", gap: ".5rem", padding: "2rem 0" }}>
       <SelectOption
         title={"دسته بندی"}
-        URL={"/categories?limit=1000"}
-        responseData={"categories"}
-        showValue={formValues?.categories}
-        onChangeSelect={(value: string) =>
-          setFormValues((pre) => ({ ...pre, categories: value }))
-        }
+        URL={`${URL_CATEGORY}?limit=1000`}
+        responseData={URL_CATEGORY}
+        showValue={formValues.category}
+        onChangeSelect={(value) => {
+          dispatch(setReq(false));
+          dispatch(setCategory(value));
+          dispatch(setSubcategory(""));
+        }}
       />
-    </form>
+      <SelectOption
+        title={"زیر مجموعه"}
+        URL={`${URL_SUBCATEGORY}?limit=1000${
+          formValues.category ? `&category=${formValues.category}` : ""
+        }`}
+        responseData={URL_SUBCATEGORY}
+        showValue={formValues.subcategory}
+        onChangeSelect={(value) => {
+          dispatch(setReq(false));
+          dispatch(setSubcategory(value));
+        }}
+      />
+      <TextField
+        id="quantity"
+        label="تعداد"
+        variant="standard"
+        color="primary"
+        type="number"
+        focused
+        sx={{ width: "100px" }}
+        onChange={(e) => {
+          dispatch(setReq(false));
+          dispatch(setQuantity(Number(e.target.value)));
+        }}
+      />
+      <TextField
+        id="price"
+        label="قیمت"
+        variant="standard"
+        sx={{ width: "150px" }}
+        type="number"
+        onChange={(e) => {
+          dispatch(setReq(false));
+          dispatch(setPrice(Number(e.target.value)));
+        }}
+        focused
+      />
+      <Button variant="text" onClick={handleSubmit} type="button">
+        جست و جو
+      </Button>
+    </Box>
   );
 };
-{
-  /* //   <BaseInput 
-    //     slots={{
-    //       root: InputRoot,
-    //       input: InputElement,
-    //     }}
-    //     id="quantity"
-    //     name="username"
-    //     onChange={()=>setFormValues(pre=>({...pre,}))}
-    //     type="text"
-    //   />*/
-}
