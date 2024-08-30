e1dc6b4106db3b88bd15c7a4b0172781868bebff
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-30 18:24:00 +0330

    feat:admin add products image slider

---
+++ src/pages/admin/admin_products/components/add_new _product/components/add_images/AAPImageSlider.tsx
@@ -0,0 +1,36 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Box } from "@mui/material";
+import { FormikProps } from "formik";
+import { ValuesIF } from "../../utils/interface";
+import { AAPIHover } from "./AAPIHover";
+import { useWindowSize } from "../../../../../../../hooks";
+// -------------------------------------------------------
+// AAPImagesSlider ===> admin add products image slider
+export const AAPImagesSlider = ({
+  formik,
+}: {
+  formik: FormikProps<ValuesIF>;
+}) => {
+    const size=useWindowSize()
+  return (
+    <Box width={'40vw'} >
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={10}
+        slidesPerView={size.width/300}
+        scrollbar={{ draggable: true }}
+      >
+        {formik?.values?.images?.map((image, index) => (
+          <SwiperSlide key={index}>
+           <AAPIHover {...{image}}/>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
