d85fb333567bc704961f951cf2008e72bf90f46d
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-01 16:47:00 +0330

    admin add product category and sub category label

--- src/pages/admin/admin_products/components/add_new _product/components/add_category_and_subcategory/AAPCASComp.tsx
+++ src/pages/admin/admin_products/components/add_new _product/components/add_category_and_subcategory/AAPCASComp.tsx
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------
 
-import { Grid } from "@mui/material";
+import { Grid, InputLabel, Stack } from "@mui/material";
 import { addSelectOptionDatas, textFieldAddSACData } from "../../config";
 import { SelectOption } from "../../../search";
 import { FC } from "react";
@@ -14,31 +14,36 @@ interface AAPCASCompIF {
 
 // AAPCASComp ==> admin add products category and subcategory component
 export const AAPCASComp: FC<AAPCASCompIF> = ({ formik }) => {
-  console.log("first", formik?.values?.category?.length > 0);
   return (
     <>
       {addSelectOptionDatas?.map((selection, index) => (
         <Grid item xs={12} sm={6} md={4} key={selection?.id}>
-          <SelectOption
-            key={selection?.id + index}
-            title={selection?.title}
-            URL={selection?.URL(formik?.values?.category)}
-            responseData={selection?.responseData}
-            showValue={formik?.values[selection?.showValue]}
-            req={
-              formik?.values?.category === "addNew"
-                ? false
-                : selection?.showValue === "category"
-                ? true
-                : formik?.values?.category?.length > 0
-                ? true
-                : false
-            }
-            addProduct={true}
-            onChangeSelect={(value) => {
-              selection?.onChange(value, formik);
-            }}
-          />
+          <Stack gap={2} id={selection?.id + index}>
+            <SelectOption
+              key={selection?.id + index}
+              title={selection?.title}
+              URL={selection?.URL(formik?.values?.category)}
+              responseData={selection?.responseData}
+              showValue={formik?.values[selection?.showValue]}
+              req={
+                formik?.values?.category === "addNew"
+                  ? false
+                  : selection?.showValue === "category"
+                  ? true
+                  : formik?.values?.category?.length > 0
+                  ? true
+                  : false
+              }
+              addProduct={true}
+              onChangeSelect={(value) => {
+                selection?.onChange(value, formik);
+              }}
+            />
+            <InputLabel htmlFor={selection?.id + index}>
+              {formik.touched[selection?.showValue] &&
+                formik.errors[selection?.showValue] }
+            </InputLabel>
+          </Stack>
         </Grid>
       ))}
       {formik?.values?.category === "addNew" &&
