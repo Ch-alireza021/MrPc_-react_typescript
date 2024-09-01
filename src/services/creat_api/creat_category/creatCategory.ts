858e5191bc4cb656b929d80f154e2d003c710265
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-01 12:33:56 +0330

    feat:add category

---
+++ src/services/creat_api/creat_category/creatCategory.ts
@@ -0,0 +1,15 @@
+import { api } from "../../interceptors";
+
+export const creatCat = async (categoryName: string) => {
+  try {
+    const resCat = await api.post("/categories", {
+      name: categoryName,
+    });
+    if (resCat.status === 201) {
+      console.log("Category added successfully!", resCat);
+      return resCat?.data?.data?.category?._id;
+    }
+  } catch (error) {
+    console.error("Error adding category:", error);
+  }
+};
---
+++ src/services/creat_api/index.ts
@@ -0,0 +1,1 @@
+export * from './creat_category'
