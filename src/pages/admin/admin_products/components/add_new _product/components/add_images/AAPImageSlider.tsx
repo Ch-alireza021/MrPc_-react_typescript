20a2e26fad384085c9ae6636e4d6f20d196b2751
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-07 17:37:36 +0330

    feat :delete image for add product

--- src/pages/admin/admin_products/components/add_new _product/components/add_images/AAPImageSlider.tsx
+++ src/pages/admin/admin_products/components/add_new _product/components/add_images/AAPImageSlider.tsx
@@ -9,6 +9,8 @@ import { FormikProps } from "formik";
 import { ValuesIF } from "../../utils/interface";
 import { AAPIHover } from "./AAPIHover";
 import { useWindowSize } from "../../../../../../../hooks";
+import { DeleteModal } from "../../../../../../../components";
+import { useState } from "react";
 // -------------------------------------------------------
 // AAPImagesSlider ===> admin add products image slider
 export const AAPImagesSlider = ({
@@ -17,6 +19,27 @@ export const AAPImagesSlider = ({
   formik: FormikProps<ValuesIF>;
 }) => {
   const size = useWindowSize();
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+  const [deleteImg, setDeleteImg] = useState<File | null>(null);
+
+  const onClose = () => {
+    console.log(openDelete);
+    setOpenDelete(false);
+  };
+  const response = (res: boolean) => {
+    if (res === true && deleteImg) {
+      const images = formik.values.images.filter(
+        (i) => i.name !== deleteImg.name
+      );
+      formik.setFieldValue("images", images);
+    }
+    setOpenDelete(false);
+    setDeleteImg(null);
+  };
+  const handleDelete = (img: File) => {
+    setOpenDelete(true);
+    setDeleteImg(img);
+  };
   return (
     <Box width={`${((size.width - 240) * 65) / size.width}vw`}>
       <Swiper
@@ -27,10 +50,15 @@ export const AAPImagesSlider = ({
       >
         {formik?.values?.images?.map((image, index) => (
           <SwiperSlide key={index}>
-            <AAPIHover {...{ image }} />
+            <AAPIHover {...{ image, handleDelete }} />
           </SwiperSlide>
         ))}
       </Swiper>
+      <DeleteModal
+        {...{ onClose, response }}
+        onOpen={openDelete}
+        data={deleteImg}
+      />
     </Box>
   );
 };
