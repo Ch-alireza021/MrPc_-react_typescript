965b93be994a781dbffd16d162af6d9b1b59ff5e
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-10 09:21:39 +0330

    feat:main new products

---
+++ src/components/home/main/HomeMain.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { MNProducts } from "./newest";
+
+export const HomeMain = () => {
+  return (
+    <Box>
+      <MNProducts />
+    </Box>
+  );
+};
