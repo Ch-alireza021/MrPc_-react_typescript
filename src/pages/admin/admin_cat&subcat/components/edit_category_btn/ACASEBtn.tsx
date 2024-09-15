f7717d4b075f34dede3464b9340cdf446b89dc5e
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-15 17:43:40 +0330

    improbve ACASEBtn

--- src/pages/admin/admin_cat&subcat/components/edit_category_btn/ACASEBtn.tsx
+++ src/pages/admin/admin_cat&subcat/components/edit_category_btn/ACASEBtn.tsx
@@ -1,23 +1,40 @@
 import Button from "@mui/material/Button";
 import VisibilityIcon from "@mui/icons-material/Edit";
 import { alpha } from "@mui/material";
+import { BasicModal } from "../../../../../components";
+import { useState } from "react";
+import { AECForm } from "./AECForm";
+import { CategoryIF } from "../../utils";
+import { AECFormDataIF } from "../../config";
 
-export const ACASEBtn = () => {
+// Define the interface with both row and data props
+export interface ACASEBtnIF {
+  row: CategoryIF;
+  data: AECFormDataIF[];
+}
+
+export const ACASEBtn = ({ row, data }: ACASEBtnIF) => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <Button
-      variant="contained"
-      color="success"
-      sx={{
-        bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
-        color: "text.secondary",
-        "&:hover": {
-          bgcolor: (theme) => alpha(theme.palette.primary.main, 0.16),
-        },
-      }}
-      //   onClick={handleEditProduct}
-      endIcon={<VisibilityIcon />}
-    >
-      ویرایش
-    </Button>
+    <>
+      <Button
+        variant="contained"
+        color="success"
+        sx={{
+          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
+          color: "text.secondary",
+          "&:hover": {
+            bgcolor: (theme) => alpha(theme.palette.primary.main, 0.16),
+          },
+        }}
+        onClick={() => setOpen(true)}
+        endIcon={<VisibilityIcon />}
+      >
+        ویرایش
+      </Button>
+      <BasicModal open={open} setOpen={setOpen}>
+        <AECForm data={data} row={row} />
+      </BasicModal>
+    </>
   );
 };
