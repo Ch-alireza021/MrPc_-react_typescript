61f8eb7dd7a4365ee318d406a138a9da5404ba94
Ch-alireza021 <ch.alireza021@gmail.com>
2024-09-21 09:47:32 +0330

    add subcategory data

--- src/pages/admin/admin_cat&subcat/config/data.tsx
+++ src/pages/admin/admin_cat&subcat/config/data.tsx
@@ -1,14 +1,16 @@
+import { URL_CATEGORY } from "../../../../config";
 import { fDate } from "../../../../utils";
+import { CASName } from "../../admin_products/components";
 import { ACASEBtn } from "../components";
 import { ACASIcon } from "../components/table/ACASIcon";
-import { CategoryDataIF, CategoryIF } from "../utils";
+import { AECFormDataIF, CategoryDataIF, CategoryIF, SubategoryDataIF } from "../utils";
 
 export const categoryData: CategoryDataIF[] = [
   {
     key: "icon",
     render: (row: CategoryIF) => <ACASIcon row={row} />,
   },
- 
+
   {
     key: "name",
     render: (row: { name: string }) => row?.name,
@@ -19,7 +21,35 @@ export const categoryData: CategoryDataIF[] = [
   },
   {
     key: "edit",
-    render: () => <ACASEBtn />,
+    render: (row: CategoryIF) => <ACASEBtn row={row} data={aECFormData} />,
   },
 ];
 // --------------------------------------------
+export const subcategoryData: SubategoryDataIF[] = [
+  {
+    key: "name",
+    render: (row: { name: string }) => row?.name,
+  },
+  {
+    key: "category",
+    render: (row: { category: string }) => (
+      <CASName {...{ URL: URL_CATEGORY, id: row?.category }} />
+    ),
+  },
+  {
+    key: "createdAt",
+    render: (row: { createdAt: string }) => fDate(row?.createdAt),
+  },
+  {
+    key: "edit",
+    render: (row) => <ACASEBtn row={row} data={[]} />,
+  },
+];
+// --------------------------------------------
+
+export const aECFormData: AECFormDataIF[] = [
+  {
+    label: "نام",
+    id: "name",
+  },
+];
