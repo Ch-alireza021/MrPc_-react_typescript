019dddefd6629e7b4526e47941a09a3cb6489744
Ch-alireza021 <ch.alireza021@gmail.com>
2024-10-06 10:32:39 +0330

    improve home main

--- src/components/home/main/HomeMain.tsx
+++ src/components/home/main/HomeMain.tsx
@@ -1,11 +1,30 @@
 import { Box } from "@mui/material";
-import React from "react";
 import { MNProducts } from "./newest";
+import { useQuery } from "@tanstack/react-query";
+import { URL_CATEGORY } from "../../../config";
+import { dpi, generalGet } from "../../../services";
+import { ShowCategories } from "./categories";
 
 export const HomeMain = () => {
+  const { data: showCatData } = useQuery({
+    // SHCData ==> show category data
+    queryKey: ["SHCData"],
+    queryFn: async () => {
+      const res = await dpi.get("HPSConfigDB");
+      const items = res.data?.[0]?.selected;
+      const promises = items.map((item: string[]) =>
+        generalGet(URL_CATEGORY + "/" + item).then((res) => res.data?.category)
+      );
+      const results = await Promise.all(promises);
+      return results;
+    },
+  });
   return (
     <Box>
       <MNProducts />
+      {showCatData?.map((i: { _id: string; name: string }) => (
+        <ShowCategories id={i._id} name={i.name} key={i._id} />
+      ))}
     </Box>
   );
 };
--- src/components/home/main/index.ts
+++ src/components/home/main/index.ts
@@ -1,1 +1,2 @@
 export * from './HomeMain'
+export * from './categories'
